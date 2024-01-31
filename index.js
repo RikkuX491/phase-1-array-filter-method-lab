ca1a1d5f35b5c0b6b47dc58b1458cd8a28c75237
Ricardo Guerra <RikkuG491@gmail.com>
2024-01-31 17:52:23 -0500

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+// Function # 1 - findMatching() function
+function findMatching(driverNamesArray, stringToMatch){
+    return driverNamesArray.filter((driverName) => {
+        return driverName.toUpperCase() === stringToMatch.toUpperCase()
+    })
+}
+
+// Function # 2 - fuzzyMatch() function
+function fuzzyMatch(driverNamesArray, stringToMatch){
+    return driverNamesArray.filter((driverName) => {
+        // return driverName.slice(0, stringToMatch.length) === stringToMatch
+        return driverName[0] === stringToMatch[0]
+    })
+}
+
+// Function # 3 - matchName() function
+function matchName(driverObjectsArray, stringToMatch){
+    return driverObjectsArray.filter((driverObject) => {
+        return driverObject.name.toLowerCase() === stringToMatch.toLowerCase()
+    })
+}
