11a1017537111191d28d893b95dbe5d63900ec09
Ricardo Guerra <RikkuG491@gmail.com>
2024-07-17 16:36:07 -0400

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+// Function # 1 solution - findMatching()
+function findMatching(driverNames, nameToMatch){
+    return driverNames.filter(driverName => {
+        return driverName.toUpperCase() === nameToMatch.toUpperCase();
+    })
+}
+
+// Function # 2 solution - fuzzyMatch() with for loop
+// function fuzzyMatch(driverNames, startsWithString){
+//     return driverNames.filter(driverName => {
+//         for(let index = 0; index < startsWithString.length; index++){
+//             if(driverName[index] !== startsWithString[index]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     })
+// }
+
+// Function # 2 solution - fuzzyMatch() using the built-in startsWith() method
+function fuzzyMatch(driverNames, startsWithString){
+    return driverNames.filter(driverName => {
+        return driverName.startsWith(startsWithString);
+    })
+}
+
+// Function # 3 solution - matchName()
+function matchName(drivers, name){
+    return drivers.filter(driver => {
+        return driver.name === name;
+    })
+}
